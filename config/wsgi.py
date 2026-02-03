d1b684ac1b63e869e8666896ad319336864ba763
lifanwar <mualifulanwar1@gmail.com>
2026-02-03 17:58:19 +0200

    Add load environtment django setup in .env with dev, staging and prod

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,16 +1,12 @@
-"""
-WSGI config for config project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
-"""
-
 import os
-
+from dotenv import load_dotenv
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+load_dotenv()
+setup = os.getenv('DJANGO_ENV', 'dev')  # Default to 'development' if not found
+if not setup:
+    raise EnvironmentError("DJANGO_ENV not set. Ensure the environment variable is configured correctly.")
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', f'config.settings.{setup}')
 
 application = get_wsgi_application()
