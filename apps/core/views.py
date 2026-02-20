3e75032a29bb0580bfada664c8583d8c40d16793
lifanwar <mualifulanwar1@gmail.com>
2026-02-21 00:07:51 +0200

    Update FBV to CBV

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render
+from django.views.generic.base import TemplateView
 
-# Create your views here.
-def home(request):
-    return render(request, "core/index.html")
+class home(TemplateView):
+        template_name = 'core/index.html'
